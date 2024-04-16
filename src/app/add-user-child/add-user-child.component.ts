424ba3627a35a989cdd10a5ee24256877e1a8089
qadir <aqadir1989@gmail.com>
2024-04-16 17:21:35 +0530

    using template reference variable and passing it to button for value

--- src/app/add-user-child/add-user-child.component.ts
+++ src/app/add-user-child/add-user-child.component.ts
@@ -1,20 +1,23 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-add-user-child',
   templateUrl: './add-user-child.component.html',
-  styleUrls: ['./add-user-child.component.css']
+  styleUrls: ['./add-user-child.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AddUserChildComponent {
 
-  userName : string = '';
+  // userName : string = '';
 
   @Output() userAdded  = new EventEmitter<string>();
 
 
 
-  onuserAdded() {
-    this.userAdded.emit(this.userName);
+  onuserAdded(input : HTMLInputElement) {
+    // this.userAdded.emit(this.userName);
+    // console.log('Template input',input.value);
+     this.userAdded.emit(input.value);
 
   }
 
