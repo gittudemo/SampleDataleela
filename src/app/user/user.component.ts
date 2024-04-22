47e27cdbc5351602bba1661e5f40587c50653d04
qadir <aqadir1989@gmail.com>
2024-04-22 20:00:23 +0530

    ng Docheck

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,11 +1,11 @@
-import { Component, Input } from '@angular/core';
+import { Component, DoCheck, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent {
+export class UserComponent implements OnInit, DoCheck{
 
 
   @Input() userName: string | undefined; 
@@ -14,9 +14,17 @@ export class UserComponent {
     console.log('constructor called');
   }
 
+
   ngOnInit() {
     console.log('ngOnInit() called');
 
   }
+
+
+  ngDoCheck() {
+    console.log('Do check called');
+
+  }
+
 
 }
