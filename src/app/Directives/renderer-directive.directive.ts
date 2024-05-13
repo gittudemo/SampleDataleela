8eefe1e43ce176b9a2b2d74ad78e392302b20b3f
qadir <aqadir1989@gmail.com>
2024-05-13 18:05:47 +0530

    using HostListener to listen to the mouseEvents in appRendererDirective

--- src/app/Directives/renderer-directive.directive.ts
+++ src/app/Directives/renderer-directive.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appRendererDirective]'
@@ -8,7 +8,20 @@ export class RendererDirectiveDirective {
   constructor(private element : ElementRef , private renderer: Renderer2) { }
 
 
-  ngOnInit() {
-    this.renderer.setStyle(this.element.nativeElement , 'background-color' ,'green')
+  // ngOnInit() {
+  //   this.renderer.setStyle(this.element.nativeElement , 'background-color' ,'green')
+  // }
+
+  @HostListener('mouseenter') onmousehover(event: Event){
+    this.renderer.setStyle(this.element.nativeElement, 'background-color', 'green')
+
+  }
+    @HostListener('mouseleave') onmouseleave(event: Event){
+    this.renderer.setStyle(this.element.nativeElement, 'background-color', 'yellow')
+
+  }
+    @HostListener('click') onclick(event: Event){
+    this.renderer.setStyle(this.element.nativeElement, 'background-color', 'red')
+
   }
 }
